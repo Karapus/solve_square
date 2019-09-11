17dc6e7fc9177ffe72133b925ffa73a99d3e674e
Karapus <evgeny.nay@outlook.com>
2019-09-11 22:12:12 +0300

    added attempts to input

--- solve_square.cpp
+++ solve_square.cpp
@@ -2,51 +2,56 @@
 #include <math.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio_ext.h>
 
 //! SS_INF_NROOTS - value returned by SolveSquare() if root is any number 
 const int SS_INF_NROOTS = -1;
 
-//! IE_PRESSISION - pressision of float comparison
-const float IE_PRESSISION = 1e-6f;
+//! IE_PRESSISION - pressision of double comparison
+const double IE_PRESSISION = 1e-6;
 
-int SolveSquare(float a, float b, float c, float *x1_p, float *x2_p);
-int SolveLineare(float a, float b, float *x_p);
-int IsEqual(float a, float b = 0);
-int ScanCoefficient(char *name, float *addr);
+int SolveSquare(double a, double b, double c, double *x1_p, double *x2_p);
+int SolveLineare(double a, double b, double *x_p);
+int IsEqual(double a, double b = 0);
+int ScanCoefficient(const char *name, double *addr);
+double *GetNewInput(const char *prompt, size_t NELEMS);
+int ScanElem(double *f);
 
 int main()
 {
 	printf("Solve sqare equation\n");
 	
-	float a = NAN, b = NAN, c = NAN;
+/*	double a = NAN, b = NAN, c = NAN;
 	
 	int errn = 0;
 	if (	(errn = ScanCoefficient("x^2",		&a)) ||
 		(errn = ScanCoefficient("x",		&b)) || 
 		(errn = ScanCoefficient("constant",	&c)) )
-		return errn;
+		return errn;*/
+	double *coeffs = GetNewInput("Input x^2, x and constant coefficients", 3);
 
-	float x1 = NAN, x2 = NAN;
+	double x1 = NAN, x2 = NAN;
 	
-	int nRoots = SolveSquare(a, b, c, &x1, &x2);
+	int nRoots = SolveSquare(coeffs[0], coeffs[1], coeffs[2], &x1, &x2);
 	switch (nRoots)
 	{
 		case 0:
-			printf("No roots\n");
+			printf("No roots of %gx^2 + %gx + %g == 0\n", coeffs[0], coeffs[1], coeffs[2]);
 			break;
 		case 1:
-			printf("Root  of %gx^2 + %gx + %g == 0 is:\n %g\n", a, b, c, x1);
+			printf("Root  of %gx^2 + %gx + %g == 0 is:\n %g\n", coeffs[0], coeffs[1], coeffs[2], x1);
 			break;
 		case 2:
-			printf("Roots of %gx^2 + %gx + %g == 0 are:\n %g %g\n", a, b, c, x1, x2);
+			printf("Roots of %gx^2 + %gx + %g == 0 are:\n %g %g\n", coeffs[0], coeffs[1], coeffs[2], x1, x2);
 			break;
 		case SS_INF_NROOTS:
-			printf("Any number is a root of %gx^2 + %gx + %g == 0\n", a, b, c);
+			printf("Any number is a root of %gx^2 + %gx + %g == 0\n", coeffs[0], coeffs[1], coeffs[2]);
 			break;
 		default:
 			fprintf(stderr, "Error in main(): Invalid number of roots\n");
 			return 2;
 	}
+	free(coeffs);
 	return 0;
 }
 
@@ -56,15 +61,63 @@ int main()
 	@param addr addres to write coefficient value
 	@return 0 if no error occurs		*/
 
-int ScanCoefficient(char *name, float *addr)
+int ScanCoefficient(const char *name, double *addr)
 {
         printf("Enter %s coefficient of equation:\n", name);
-        if (!scanf("%g", addr))
+        if (!scanf("%lg", addr))
         {
                 fprintf(stderr, "Error in ScanCoefficient(): Invalid coeficient %s\n", name);
                 return 1;
         }
 	return 0;
+}
+
+/*! GetNewInput
+	@note function to read NELEMS doubles from stdin
+	@param prompt message to show before reading
+	@param NELEMS number of doubles to read
+	@return array of NELEMS doubles		*/
+
+double *GetNewInput(const char *prompt, const size_t NELEMS)
+{
+	puts(prompt);
+        double *data = (double *) calloc(NELEMS, sizeof(*data));
+
+        size_t i = 0;
+        while (i < NELEMS)
+        {
+                if (ScanElem(data + i))
+                {
+                        printf("Can`t scan element number %ld\n", i);
+                        break;
+                }
+                i++;
+        }
+
+        return data;
+}
+
+/*! ScanElem
+	@note function to scan double from stdin
+	@param f pointer to double to write in
+	@return 0 if double is red		*/
+
+int ScanElem(double *f)
+{
+        const unsigned NTRIES = 10;	/*!maximum number of attempts to read f*/
+        for (unsigned i = 0; i < NTRIES; i++)
+        {
+                //printf("tryn %d\n", i);
+                if (scanf("%lg", f))
+                {
+                        //puts("Ok");
+                        return 0;
+                }
+                //fflush(stdin);
+                __fpurge(stdin);
+		printf("Invalid coefficient, type again. %d attempt%s remain%s\n", NTRIES - i - 1, i < NTRIES - 2 ? "s" : "", i < NTRIES - 2 ? "" : "s");
+        }
+        return 1;
 }
 
 /*! SolveSquare
@@ -76,13 +129,13 @@ int ScanCoefficient(char *name, float *addr)
 	@param x2_p addres of 2-nd root
 	@return number of roots			*/
 
-int SolveSquare(float a, float b, float c, float *x1_p, float *x2_p)
+int SolveSquare(double a, double b, double c, double *x1_p, double *x2_p)
 {
 	assert(isfinite(a));
 	assert(isfinite(b));
 	assert(isfinite(c));
-	assert(x1_p != NULL);
-	assert(x2_p != NULL);
+	assert(x1_p != nullptr);
+	assert(x2_p != nullptr);
 	assert(x1_p != x2_p);
 	
 	if (IsEqual(a, 0))		/*solve linear equation*/
@@ -96,7 +149,7 @@ int SolveSquare(float a, float b, float c, float *x1_p, float *x2_p)
 		return SolveLineare(a, b, x2_p);
 	}
 
-	float d = b*b - 4*a*c;		/*solve sqare equation*/
+	double d = b*b - 4*a*c;		/*solve sqare equation*/
 	if (IsEqual(d, 0))
 	{
 		*x1_p = -b / (2 * a);
@@ -115,11 +168,11 @@ int SolveSquare(float a, float b, float c, float *x1_p, float *x2_p)
 	@param x_p adress of root
 	@return number of roots			*/
 
-int SolveLineare(float a, float b, float *x_p)
+int SolveLineare(double a, double b, double *x_p)
 {
 	assert(isfinite(a));
 	assert(isfinite(a));
-	assert(x_p != NULL);
+	assert(x_p != nullptr);
 
 	if (IsEqual(a, 0)) return IsEqual(b, 0) ? SS_INF_NROOTS : 0;
 	*x_p = -b / a;
@@ -127,12 +180,12 @@ int SolveLineare(float a, float b, float *x_p)
 }
 
 /*!IsEqual
-	@note compare to floats
-	@param a first float
-	@param b second float
+	@note compare to doubles
+	@param a first double
+	@param b second double
 	@return 0 if a != b	*/
 
-int IsEqual(float a, float b)
+int IsEqual(double a, double b)
 {
         return fabs(a - b) < IE_PRESSISION;
 }
